693452a034441522fbc85200dbe7f07814bdcb2c
cjlee1 <cjlee@ualberta.ca>
2019-02-20 11:56:46 -0500

    simple login feature

--- backend/project404_t8/project404_t8/settings.py
+++ backend/project404_t8/project404_t8/settings.py
@@ -118,4 +118,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-LOGIN_REDIRECT_URL = '/'
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
