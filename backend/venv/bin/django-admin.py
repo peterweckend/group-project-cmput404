f45252289327520deb6df2a305acec3309fd2cfb
cjlee1 <cjlee@ualberta.ca>
2019-02-22 19:52:26 -0500

    pip install django-tastypie for requirements and basic restful api

--- backend/venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/student/Documents/cmput404/project1/group-project-cmput404/backend/venv/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
