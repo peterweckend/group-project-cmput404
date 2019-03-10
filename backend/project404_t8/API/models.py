5178225e4a665f23d2075cebb386f2ad304b6afb
PeterWeckend <pweckend@ualberta.ca>
2019-03-10 17:51:46 -0600

    Update models.py

--- backend/project404_t8/API/models.py
+++ backend/project404_t8/API/models.py
@@ -1,11 +1,18 @@
 from django.db import models
 
 # Create your models here.
-# Create your models here.
-class Posts(models.Model):
+class Post(models.Model):
     id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=250)
     body = models.TextField()
 
     def __str__(self):
             return '%s %s' % (self.title, self.body)
+
+
+class User(models.Model):
+    id = models.AutoField
+
+
+class Server(models.Model):
+
