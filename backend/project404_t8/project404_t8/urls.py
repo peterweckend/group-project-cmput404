cd697e3bd858f1138eb53913bc58163b4864b4dc
Benjamin Hlewka <bhlewka@cs.ualberta.ca>
2019-03-11 14:22:28 -0600

    upload, forms, views

--- backend/project404_t8/project404_t8/urls.py
+++ backend/project404_t8/project404_t8/urls.py
@@ -18,11 +18,14 @@ from django.urls import path, include
 from django.views.generic.base import TemplateView # new
 from django.conf.urls import url, include
 from .router import router
+from .views import *
+
 # from API import urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
     path('', TemplateView.as_view(template_name='home.html'), name='home'), # new
+    path("upload/", uploadView, name="upload"),
     url(r'^API/', include(router.urls)),
 ]
