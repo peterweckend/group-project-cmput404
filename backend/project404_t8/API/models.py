79e7900c75b48950597c9206c135f4f95d6b7118
PeterWeckend <pweckend@ualberta.ca>
2019-03-10 19:58:00 -0600

    Added extra user fields, added serializers (pair programmed with @tiakindele)

--- backend/project404_t8/API/models.py
+++ backend/project404_t8/API/models.py
@@ -5,6 +5,9 @@ from django.db import models
 
 class User(models.Model):
     id = models.AutoField(primary_key=True)
+    username = models.TextField(default='Username')
+    display_name = models.TextField(default='User')
+    password = models.TextField(default='password')
 
     def __str__(self):
         return '%s' % (self.id)
