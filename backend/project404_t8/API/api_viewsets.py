fc81fad07b80d407b2073f59fb5c1fedebcc36ff
James Vo <jamesvo@Jamess-MacBook-Air.local>
2019-03-26 16:49:41 -0600

    fixed pagination for post comments

--- backend/project404_t8/API/api_viewsets.py
+++ backend/project404_t8/API/api_viewsets.py
@@ -39,8 +39,7 @@ import dateutil.parser as parser
 # Get author info
 # extra is a boolean that returns list of friends as well as github,bio,etc.
 # pk is the authors ID
-# in theory, githubRequired shouldn't be true if extra is true
-def getAuthorData(request, extra=False, pk=None, githubRequired=False):
+def getAuthorData(request, extra=False, pk=None):
     
     # Modify the requests path
     request_path = "/author/" + str(pk)
@@ -48,15 +47,6 @@ def getAuthorData(request, extra=False, pk=None, githubRequired=False):
     queryset = CustomUser.objects.all()
     user = get_object_or_404(queryset, pk=pk)
     response = {}
-    
-    response["id"] = "http://" + request.get_host() + request_path
-    # todo: look up the user, find what host they belong to, and return that value
-    # instead of using request.get_host() here
-    response["host"] = request.get_host()
-    response["displayName"] = user.displayname
-    response["url"] = "http://" + request.get_host() + request_path
-    if githubRequired:
-        response["github"] = user.github_url
 
     # build a list of friends for the response
     # This will be optional
@@ -89,6 +79,15 @@ def getAuthorData(request, extra=False, pk=None, githubRequired=False):
             response["email"] = user.email
         if Services.isNotBlank(user.bio):
             response["bio"] = user.bio
+
+    
+    response["id"] = "http://" + request.get_host() + request_path
+    # todo: look up the user, find what host they belong to, and return that value
+    # instead of using request.get_host() here
+    response["host"] = request.get_host()
+    response["displayName"] = user.displayname
+    response["url"] = "http://" + request.get_host() + request_path
+    # do we need github link down here? ex: in the comments request
     
     return response
 
@@ -102,7 +101,7 @@ def getCommentData(request, pk=None):
     response = OrderedDict()
 
     author_id = int(comment["author"])
-    author_response = getAuthorData(request, extra=False, pk=author_id, githubRequired=True)
+    author_response = getAuthorData(request, extra=False, pk=author_id)
     response.update({"author":author_response})
     response.update({"comment":comment["body"]})
     if comment["is_markdown"]:
@@ -162,26 +161,26 @@ def getPostData(request, pk=None):
     # Get author information
     # Then add author to the dic
     authorId = str(post["author"])
-    author = getAuthorData(request, extra=False, pk=authorId, githubRequired=True)
+    author = getAuthorData(request, extra=False, pk=authorId)
     currentPost.update({"author":author})
 
     # categories
     # TODO: go into the categories table, find all entries associated with this post
     # and put them into a list format, and add them to the response here
-    post_categories = ["dont", "exist", "yet"]
-    currentPost.update({"categories":post_categories})
+    
+    # Get comment info
 
     # TODO: add the total number of comments, and page size, and next, 
     # previous, and all that other pagination stuff here
+
+    # TODO peter you should be able to figure this out
+    # Use the helper function you will make
+    # This actually looks so tedious omg
     # From spec:
     # You should return ~ 5 comments per post.
 	# should be sorted newest(first) to oldest(last)
-    queryset = Comment.objects.filter(post=pk)
-    comments = CommentSerializer(queryset, many=True).data
-    comments_response = []
-    for comment in comments:
-        comments_response.append(getCommentData(request, pk=comment["id"]))
-    currentPost.update({"comments":comments_response})
+    comments = []
+    currentPost.update({"comments":comments})
 
     published = parser.parse(post["published"]) # ISO 8601 format
     currentPost.update({"published":published.isoformat()})
@@ -189,13 +188,7 @@ def getPostData(request, pk=None):
     currentPost.update({"id":post["id"]})
     
     # visibility ["PUBLIC","FOAF","FRIENDS","PRIVATE","SERVERONLY"]
-    currentPost.update({"visibility":Services.get_privacy_string_for_post(post["privacy_setting"])})
-
-    # todo: waiting on the ability for multiple private authors
-    currentPost.update({"visibleTo":"..."})
-
-    # todo: waiting until the post as an isUnlisted boolean attribute
-    currentPost.update({"unlisted":"..."})
+    currentPost.update({"visibility":"..."})
 
     return currentPost
 
@@ -292,7 +285,6 @@ class PostsViewSet(viewsets.ModelViewSet):
         # Finally, return this huge mfer
         return Response(response)
     
-    
     # GET http://service/posts/{POST_ID} access to a single post with id = {POST_ID}
     def retrieve(self, request, pk=None):
         # permission_classes = (IsAuthenticated,)
@@ -300,7 +292,6 @@ class PostsViewSet(viewsets.ModelViewSet):
         serializer_class = PostSerializer(queryset, many=True)
         return Response(serializer_class.data)
     
-
     # the API endpoint accessible at GET http://service/posts/{post_id}/comments
     @action(methods=['get','post'], detail=True, url_path="comments")
     def userPostComments(self, request, pk=None):
@@ -310,12 +301,10 @@ class PostsViewSet(viewsets.ModelViewSet):
             # check that we're allowed to see the post - for now just check if the posts are public
             # for right now, just return comments from public posts
             if requested_post.privacy_setting == "6": 
-                # todo: create a new comment object and return the right json response mentioned in spec
                 queryset = Comment.objects.filter(post=post_id)
                 serializer_class = CommentSerializer(queryset, many=True)
                 return Response(serializer_class.data)
             else:
-                # todo: respond with 403 forbidden as well as the right json response mentioned in spec
                 raise PermissionDenied("Forbidden: The post you wished to access comments for is not Public")
 
         elif request.method == "GET": # this handles "GET" methods
@@ -331,7 +320,7 @@ class PostsViewSet(viewsets.ModelViewSet):
                 for comment in comments:
                     comments_response.append(getCommentData(request, pk=comment["id"]))
 
-                paginated_posts = paginator.paginate_queryset(comments_response, request)
+                paginated_comments = paginator.paginate_queryset(comments_response, request)
 
                 response = OrderedDict()
                 response.update({"query":"comments"})
@@ -339,7 +328,7 @@ class PostsViewSet(viewsets.ModelViewSet):
                 response.update({"size": Services.get_page_size(request, paginator)})
                 response.update({"next": None})
                 response.update({"previous": None})
-                response.update({"comments":comments_response})
+                response.update({"comments":paginated_comments})
 
                 if paginator.get_next_link() is not None:
                     response["next"] = paginator.get_next_link()
@@ -366,17 +355,53 @@ class AuthorViewSet(viewsets.ModelViewSet):
     def list(self, request):
         raise NotFound()
 
-
     # we don't want there to be any functionality for GET http://service/author
     def create(self, request):
         raise NotFound()
 
-
     # GET http://service/author/{author_id}
     # returns information about the author
     def retrieve(self, request, pk=None):
-        return Response(getAuthorData(request, extra=True, pk=pk, githubRequired=False))
+        queryset = CustomUser.objects.all()
+        user = get_object_or_404(queryset, pk=pk)
+        # paginator = PostsPagination()
+        # posts = paginator.paginate_queryset(queryset, request)
 
+        # build a list of friends for the response
+        friends_list = []
+        friends = Friendship.objects.filter(friend_a=user.id)
+        for friend in friends:
+            friend_entry = {}
+
+            url = "https://" + request.get_host() + "/author/" + str(friend.friend_b.id) 
+            friend_object = get_object_or_404(queryset, pk=friend.friend_b.id)
+
+            friend_entry["id"] = url
+            # todo: look up the user, find what host they belong to, and return that value
+            # instead of using request.get_host() here
+            friend_entry["host"] = "https://" + request.get_host() + "/" 
+            friend_entry["displayName"] =  friend_object.displayname
+            friend_entry["url"] = url
+            friends_list.append(friend_entry)
+
+        response = {}
+        response["id"] = "http://" + request.get_host() + request.get_full_path()
+        response["host"] = request.get_host()
+        response["displayName"] = user.displayname
+        response["url"] = "http://" + request.get_host() + request.get_full_path()
+        response["friends"] = friends_list
+        if Services.isNotBlank(user.github_url):
+            response["github"] = user.github_url
+        if Services.isNotBlank(user.first_name):
+            response["firstName"] = user.first_name
+        if Services.isNotBlank(user.last_name):
+            response["lastName"] = user.last_name
+        if Services.isNotBlank(user.email):
+            response["email"] = user.email
+        if Services.isNotBlank(user.bio):
+            response["bio"] = user.bio
+
+        return Response(response)
 
     # http://service/author/posts (posts that are visible to the currently authenticated user)
     @action(methods=['get'], detail=False)
@@ -400,7 +425,6 @@ class AuthorViewSet(viewsets.ModelViewSet):
 
         serializer_class = PostSerializer(allowed_posts, many=True)
         return Response(serializer_class.data)
-
 
     # the API endpoint accessible at GET http://service/author/{author_id}/posts
     # Can't name this method "posts" because there's already a "posts" method above
@@ -445,10 +469,9 @@ class AuthorViewSet(viewsets.ModelViewSet):
         # print(paginator.get_next_link())
         return Response(response)
 
-
     # the API endpoint accessible at GET http://service/author/<authorid>/friends/
     # returns the author's friend list
-    # TODO for Peter: add functionality for POSTs as well as GETs
+    # TODO rename this function, its got a duplicate name basically
     @action(methods=['get'], detail=True, url_path="friends")
     def userFriends(self, request, pk=None):
         author_id = pk
@@ -470,8 +493,6 @@ class AuthorViewSet(viewsets.ModelViewSet):
     
         # return serialized friendship_authors
         return Response(friendship_dict)
-
-
     @action(methods=['get'], detail=True, url_path="friends/(?P<author_id2>\d+)")
     def friends(self, request, pk=None,author_id2=None):
         author_id = pk
