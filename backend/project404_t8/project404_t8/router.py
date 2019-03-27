5a69482677f7f35fdfb2d517f79ec080c960f22b
PeterWeckend <pweckend@ualberta.ca>
2019-03-27 16:25:46 -0600

    updated trailing slash

--- backend/project404_t8/project404_t8/router.py
+++ backend/project404_t8/project404_t8/router.py
@@ -2,7 +2,7 @@ from rest_framework import routers
 import API.viewsets as Viewsets
 
 # these are the API methods
-api_router = routers.SimpleRouter()
+api_router = routers.SimpleRouter(trailing_slash=False)
 api_router.register(r'posts', Viewsets.PostsViewSet)
 api_router.register(r'author', Viewsets.AuthorViewSet)
 api_router.register(r'friendrequest', Viewsets.FriendRequestViewSet)
