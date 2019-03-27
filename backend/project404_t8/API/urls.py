1ae7f1a37ae77b856158910be5a4fc0559eddd76
PeterWeckend <pweckend@ualberta.ca>
2019-03-27 15:52:41 -0600

    Updated friend request url

--- backend/project404_t8/API/urls.py
+++ backend/project404_t8/API/urls.py
@@ -15,5 +15,5 @@ urlpatterns =[
     # path("profile/", profileView, name="profile"),
     url(r'^profile/(?P<username>[a-zA-Z0-9]+)$', profileView, name='profile'),
     url(r'^editprofile/(?P<pk>\d+)/$', editProfile.as_view(), name='editprofile'),
-    path("friendrequest2/", friendRequestView, name="Friend Request"),
+    path("addFriend/", friendRequestView, name="Friend Request"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
