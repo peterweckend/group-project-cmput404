370a4284cd168b8125cb2e7f7591dfe247ae2738
Tolu Akindele <tiakindele11@gmail.com>
2019-03-28 03:20:11 -0600

    Filter posts for 'Private to my server' privacy setting #71 FIXED

--- backend/project404_t8/API/viewsets_sql_query.sql
+++ backend/project404_t8/API/viewsets_sql_query.sql
@@ -23,16 +23,18 @@ WITH posts AS
     
     UNION
     
-    -- >>>>>>>>>> gets posts satisfing #1, #3, #6 <<<<<<<<<<
+    -- >>>>>>>>>> gets posts satisfing #1, #3, #5, #6 <<<<<<<<<<
     SELECT id FROM API_post WHERE 
         (author_id in  
             (WITH friends(fid) AS 
                 (SELECT friend_b_id FROM API_friendship WHERE friend_a_id=%s) 
                 SELECT * FROM friends WHERE fid != %s GROUP BY fid)
             AND 
-            (privacy_setting = 3 OR privacy_setting = 4)) 
-            -- should be able to just add an OR (privacy setting = 5 and the two host infos match)
-        OR author_id = %s OR  privacy_setting = 6)
+            (   privacy_setting = 3 OR -- dealing with #3
+                privacy_setting = 4 OR
+            (   privacy_setting = 5 AND original_host = --dealing with #5
+            (select host from users_customuser where id = %s))))
+        OR author_id = %s OR  privacy_setting = 6) -- dealing with #1 and #6
     -- >>>>>>>>>> -------------------------------- <<<<<<<<<<
 
 SELECT * FROM API_post WHERE id in posts 
